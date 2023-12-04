efe0ffacb51af096c03f250754845614b45b2f20
HubulovaVI <valeriahb@mail.ru>
2023-12-05 03:12:29 +0500

    Task6

---
+++ Tyuiu.HubulovaVI.Sprint2.Task6.V1/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.HubulovaVI.Sprint2.Task6.V1.Lib;
+
+namespace Tyuiu.HubulovaVI.Sprint2.Task6.V1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #2 | Выполнила Хубулова В. И. | АСОИУб-23-2";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Алгоритмы разветвляющейся структуры                               *");
+            Console.WriteLine("* Задание #6                                                              *");
+            Console.WriteLine("* Вариант #1                                                              *");
+            Console.WriteLine("* Выполнила: Хубулова Валерия Игоревна | АСОИУб-23-2                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая использует сокращенную форму записи         *");
+            Console.WriteLine("* оператора switch вычисляет требуемое значение и возвращает результат.   *");
+            Console.WriteLine("* По данному номеру месяца, определите количество дней в этом месяце.     *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            string res;
+
+            int value;
+
+            Console.WriteLine("Введите значение переменной K: ");
+            value = Convert.ToInt32(Console.ReadLine());
+
+            if ((value < 1) || (value > 12))
+            {
+                res = "Введены неверные значения";
+            }
+            else
+            {
+                res = "Дней в этом месяце " + ds.FindMonthDaysCount(value);
+            }
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine(res);
+            Console.ReadKey();
+        }
+    }
+}
