bc23fa5d182ebe2d11a94c499bbfedeb70cea33d
HubulovaVI <valeriahb@mail.ru>
2023-12-05 02:56:19 +0500

    Task5

---
+++ Tyuiu.HubulovaVI.Sprint2.Task5.V14.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.HubulovaVI.Sprint2.Task5.V14.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.HubulovaVI.Sprint2.Task5.V14.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c7df5b82-6189-4e78-b667-e24437c1b9b6")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.HubulovaVI.Sprint2.Task5.V14/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.HubulovaVI.Sprint2.Task5.V14.Lib;
+
+namespace Tyuiu.HubulovaVI.Sprint2.Task5.V14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            int k, d, n;
+
+            Console.Title = "Спринт #2 | Выполнила: Хубулова В. И. | АСОИУб-23-2";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Оператор if – полная и короткая форма записи                      *");
+            Console.WriteLine("* Задание #5                                                              *");
+            Console.WriteLine("* Вариант #14                                                             *");
+            Console.WriteLine("* Выполнила: Хубулова Валерия Игоревна | АСОИУб-23-2                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая использует оператор switch вычисляет        *");
+            Console.WriteLine("* требуемое значение и возвращает результат.                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            do
+            {
+                Console.WriteLine("Введите номер дня не високосного года ");
+                k = Convert.ToInt32(Console.ReadLine());
+            } while (k <= 0 || k >= 366);
+
+
+            do
+            {
+                Console.Write("Введите номер дня недели 1 января d = ");
+                d = Convert.ToInt32(Console.ReadLine());
+            } while (d < 1 || d > 7);
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(ds.FindDayName(k, d));
+            Console.ReadKey();
+
+        }
+    }
+}
